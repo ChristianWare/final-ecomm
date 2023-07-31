c1d4395002e1317a12e5e698a8f0a73f2a74e0a1
Chris Ware <c.p.ware@hotmail.com>
2023-07-31 13:55:39 -0700

    Enviornment Variables setup

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'final-ecomm',
 
-  projectId: 'jri72r2i',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
+  dataset: process.env.SANITY_STUDIO_DATASET as string,
 
   plugins: [deskTool(), visionTool()],
 
