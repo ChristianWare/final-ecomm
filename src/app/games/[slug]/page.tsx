c8e86da0eaeb42c847247bda28a1a81cc810784b
Chris Ware <c.p.ware@hotmail.com>
2023-07-30 18:49:46 -0700

    Nested Layouts

---
+++ src/app/games/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const GameItem: NextPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Game Item Page</h1>
+    </div>
+  );
+};
+export default GameItem;
---
+++ src/app/games/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Games: NextPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1 className="text-xl">Games Page</h1>
+    </div>
+  );
+};
+export default Games;
