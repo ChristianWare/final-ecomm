c2336661f0bd8ac14c42bf50889237e837c613af
Chris Ware <c.p.ware@hotmail.com>
2023-08-02 13:12:31 -0700

    Game card + data from CMS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,16 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
-import { getCategories } from "@/libs/apis";
+import { getCategories, getGames } from "@/libs/apis";
 import Link from "next/link";
 import Image from "next/image";
+import GameCard from "@/components/GameCard/GameCard";
 
 export default async function Home() {
   const categories = await getCategories();
+  const games = await getGames();
 
-  console.log(categories);
+  // the follwing two below return an array, so you would just map through them like you did with "games"
+  const isTrendingGames = games?.filter((game) => game.isTrending);
+  const isFeaturedGame = games?.find((game) => game.isFeatured);
 
   return (
     <main>
@@ -14,8 +18,8 @@ export default async function Home() {
       <h3 className='text-4xl text-white mb-10'>Category Section</h3>
       {categories.map((category) => (
         <Link
-          href={`games/${category.slug.current}`}
           key={category._id}
+          href={`games/${category.slug.current}`}
           className='mb-5'
         >
           <Image
@@ -28,6 +32,18 @@ export default async function Home() {
           <p className='text-3xl text-white'>{category.name}</p>
         </Link>
       ))}
+
+      <div className='flex gap-8 flex-wrap mt-8'>
+        {games.map((game) => (
+          <GameCard
+            key={game._id}
+            gameName={game.name}
+            imageUrl={game.images[0].url}
+            slug={game.slug.current}
+            price={game.price}
+          />
+        ))}
+      </div>
     </main>
   );
 }
--- frontend/src/libs/apis.ts
+++ frontend/src/libs/apis.ts
@@ -1,5 +1,6 @@
 import { Category } from "@/models/category";
 import sanityClient from "./sanity";
+import { Game } from "@/models/game";
 
 export const getCategories = async (): Promise<Category[]> => {
   const query = `*[_type == "category"] {
@@ -13,4 +14,27 @@ export const getCategories = async (): Promise<Category[]> => {
   const catgories: Category[] = await sanityClient.fetch({ query });
 
   return catgories;
+};
+
+export const getGames = async (): Promise<Game[]> => {
+  const query = `*[_type == "game"] {
+    name,
+    price,
+    images,
+    isFeatured,
+    isTrending,
+    'category': *[_id == ^.category._ref][0] {
+      name,
+      slug {
+        current
+      }
+    },
+    slug,
+    quantity,
+    description
+  }`;
+
+  const games: Game[] = await sanityClient.fetch({ query });
+
+  return games;
 };
